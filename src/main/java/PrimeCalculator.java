b4c87a0e040e3c081c8416b9e38bd64cf0aee17c
Parth Purvesh <parth29dec@gmail.com>
2023-10-10 18:46:53 +0530

    Tests passed

--- src/main/java/PrimeCalculator.java
+++ src/main/java/PrimeCalculator.java
@@ -1,7 +1,20 @@
+import java.util.stream.IntStream;
+
 class PrimeCalculator {
 
     int nth(int nth) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (nth < 1) throw new IllegalArgumentException();
+        if (nth == 1) return 2;
+        if (nth == 2) return 3;
+        return IntStream.iterate(3, n -> n + 2)
+                .filter(this::isPrime)
+                .skip(nth - 2)
+                .limit(1)
+                .findFirst()
+                .getAsInt();
     }
-
+    private boolean isPrime(int n) {
+        return IntStream.rangeClosed(2, (int) Math.sqrt(n))
+                .noneMatch(i -> n % i == 0);
+    }
 }
